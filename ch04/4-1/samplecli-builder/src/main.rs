b630bb2f35349a302fb050271cdc5967e44f0975
makinzm <yuruseba@yahoo.co.jp>
2023-05-06 10:07:34 +0900

    App::new

--- ch04/4-1/samplecli-builder/src/main.rs
+++ ch04/4-1/samplecli-builder/src/main.rs
@@ -3,14 +3,14 @@ use clap::{arg, App};
 fn main() {
     let matches = App::new("My RPN program")
         .version("1.0.0")
-        .author("Your name")
+        .author("Nozomi Maki")
         .about("Super awesome sample RPN calculator")
         .arg(arg!([FILE] "Formulas written in RPN").required(false))
         .arg(arg!(-v --verbose ... "Sets the level of verbosity").required(false))
         .get_matches();
 
     match matches.value_of("FILE") {
-        Some(file) => println!("File specified: {}", file),
+        Some(sth) => println!("File specified: {}", sth),
         None => println!("No file specified."),
     }
     let verbose = matches.is_present("verbose");
