975e11bb037f0e2b88cf930dcba115141383bea2
makinzm <yuruseba@yahoo.co.jp>
2023-05-06 10:13:02 +0900

    derive macro

--- ch04/4-1/samplecli-derivemacro/src/main.rs
+++ ch04/4-1/samplecli-derivemacro/src/main.rs
@@ -15,14 +15,23 @@ struct Opts {
     /// Formulas written in RPN
     #[clap(name = "FILE")]
     formula_file: Option<String>,
+
+    /// Formulas written in RPN
+    #[clap(name = "DIF")]
+    formula_dif: Option<String>,
 }
 
 fn main() {
     let opts = Opts::parse();
 
     match opts.formula_file {
-        Some(file) => println!("File specified: {}", file),
+        Some(sth) => println!("File specified: {}", sth),
         None => println!("No file specified."),
     }
+    match opts.formula_dif {
+        Some(sth) => println!("Dif: {}", sth),
+        None => println!("No in Dif."),
+    }
+
     println!("Is verbosity specified?: {}", opts.verbose);
 }
